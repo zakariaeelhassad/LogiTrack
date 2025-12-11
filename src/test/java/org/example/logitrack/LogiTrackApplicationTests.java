1baa0baacbc1ce441965aeb5e1c29b0929eb7595
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-12-11 09:24:22 +0100

    Ajouter les tests de l'application

---
+++ src/test/java/org/example/logitrack/LogiTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.logitrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
